8b4ef1f95bc2def4334e9410192a2ab21e5945d1
vagrant <vagrant@vagrant.vm>
2017-11-08 20:01:53 +0000

    less cover

--- test_rpn.py
+++ test_rpn.py
@@ -6,13 +6,4 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
-	def test_sub(self):
-		result = rpn.calculate('5 3 -')
-		self.assertEqual(2, result)
-	def test_carat(self):
-		result = rpn.calculate('5 2 ^')
-		self.assertEqual(25, result)
-	def test_carat(self):
-		result = rpn.calculate('5 2 *')
-		self.assertEqual(10, result)
 
